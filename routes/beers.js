579219cb3386d7f444ebe22041a105d31344e5cc
jnsmtnr <janosmaitner@gmail.com>
2021-11-01 11:35:47 +0100

    add new beer endpoint

--- routes/beers.js
+++ routes/beers.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { auth } from '../middleware/auth.js'
+import { auth, isAdmin } from '../middleware/auth.js'
 import getClient from '../mongodb.js'
 
 const router = Router()
@@ -15,6 +15,28 @@ router.get('/', auth, async function (req, res) {
         const allBeers = await beers.find().toArray()
 
         res.send(allBeers);
+    }
+    catch (error) {
+        res.status(401).send({ message: error.message })
+    }
+    finally {
+        client.close()
+    }
+})
+
+router.post('/', auth, isAdmin, async function (req, res) {
+    const client = getClient()
+
+    const { name, brewery, alc, type } = req.body
+
+    try {
+        await client.connect()
+
+        const beers = client.db().collection('beers')
+
+        await beers.insertOne({ name, brewery, alc, type })
+
+        res.sendStatus(201)
     }
     catch (error) {
         res.status(401).send({ message: error.message })
