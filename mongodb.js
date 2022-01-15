4fbaf14891c83f4260d521e83ce2e07adf09d3ae
jnsmtnr <janosmaitner@gmail.com>
2022-01-15 11:56:54 +0100

    use test db for dev

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,9 @@
 import { MongoClient } from 'mongodb'
 
 const password = process.env.MONGO_PASSWORD
+const database = process.env.MONGO_DATABASE
 
-const uri = `mongodb+srv://admin:${password}@cluster0.jwoag.mongodb.net/sorcsapp?retryWrites=true&w=majority`
+const uri = `mongodb+srv://admin:${password}@cluster0.jwoag.mongodb.net/${database}?retryWrites=true&w=majority`
 
 export default function getClient() {
     return new MongoClient(uri, {
